1aaf66f8b887587641882fc3364289f1d569e445
Harnoor Kaur Dhiman <161191385+harnoor8616@users.noreply.github.com>
2026-07-01 16:08:42 +0530

    Time: 0 ms (100%), Space: 43.5 MB (72%) - LeetHub

---
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAcronym(vector<string>& words, string s) {
+        if(words.size()!=s.length())
+        {
+            return false;
+        }
+        for(int i=0;i<words.size();i++)
+        {
+            if(s[i]!=words[i][0])
+            {
+                return false;
+            }
+        }
+            return true;
+
+    }
+};
