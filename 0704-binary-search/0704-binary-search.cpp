70894f7bfb72128cb1676105425719bcdd8b96cc
Harnoor Kaur Dhiman <161191385+harnoor8616@users.noreply.github.com>
2026-07-10 22:21:30 +0530

    Time: 0 ms (100%), Space: 31.2 MB (81.51%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,20 +4,15 @@ public:
 
         int s = 0;
         int e = nums.size() - 1;
-
         while (s <= e) {
-
             int m = s + (e - s) / 2;
-
             if (nums[m] == target)
                 return m;
-
             if (target > nums[m])
                 s = m + 1;
             else
                 e = m - 1;
         }
-
         return -1;
     }
 };
