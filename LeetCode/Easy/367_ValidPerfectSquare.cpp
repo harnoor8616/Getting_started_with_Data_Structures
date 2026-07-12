355a3ab01145440b93989e90d0827d41700a4ddb
Harnoor Kaur Dhiman <161191385+harnoor8616@users.noreply.github.com>
2026-07-13 00:28:26 +0530

    Sync LeetCode - 367. Valid Perfect Square (Easy)

---
+++ LeetCode/Easy/367_ValidPerfectSquare.cpp
@@ -0,0 +1,34 @@
+/**
+ * Problem Link : https://leetcode.com/problems/valid-perfect-square/
+ * Platform     : LeetCode
+ * Difficulty   : Easy
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int s=0,e=num,m=0;
+        while(s<=e)
+        {
+            m=s+(e-s)/2;
+            long long sq=1LL * m*m;
+            if(sq==num)
+            {
+                return true;
+            }
+            else if(sq<num)
+            {
+                s=m+1;
+            }
+            else
+            {
+                e=m-1;
+            }
+
+        }
+        return false;
+    }
+};
