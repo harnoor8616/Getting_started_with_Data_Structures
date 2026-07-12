49d2ae4be715033cc99ed086b6f910d4d35d8ce0
Harnoor Kaur Dhiman <161191385+harnoor8616@users.noreply.github.com>
2026-07-13 00:16:55 +0530

    Sync LeetCode - 69. Sqrt(x) (Easy)

--- LeetCode/Easy/69_Sqrtx.cpp
+++ LeetCode/Easy/69_Sqrtx.cpp
@@ -10,23 +10,26 @@ using namespace std;
 class Solution {
 public:
     int mySqrt(int x) {
-        int s=0,e=x;
+        int s=0,e=x,m=0;
         while(s<=e)
         {
-            int m = s+(e-s)/2;
-            if(m*m==x)
+            m=s+(e-s)/2;
+            long long sq=1LL * m *m;
+            if(sq==x)
             {
                 return m;
             }
-            else if(m*m > x)
-            {
-                e=m-1;
-            }
-            else if(m*m < x)
+            else if(sq<x)
             {
                 s=m+1;
             }
+            else 
+            {
+                e=m-1;
+            }
+
         }
-        return m-1;
+        return e;
     }
+
 };
