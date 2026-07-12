c9ef28c5be866fec45085e917205349d2e132fb2
Harnoor Kaur Dhiman <161191385+harnoor8616@users.noreply.github.com>
2026-07-12 22:47:16 +0530

    Sync LeetCode - 33. Search in Rotated Sorted Array (Medium)

---
+++ LeetCode/Medium/33_SearchInRotatedSortedArray.cpp
@@ -0,0 +1,31 @@
+/**
+ * Problem Link : https://leetcode.com/problems/search-in-rotated-sorted-array/
+ * Platform     : LeetCode
+ * Difficulty   : Medium
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int s=0;e=nums.size()-1,m=0;
+        while(s<=e)
+        {
+            m=s+(e-s)/2;
+            if(nums[m]==target)
+            {
+                return m;
+            }
+            else if(nums[m]<target)
+            {
+                s=m+1;
+            }
+            else {
+                e=m-1;
+            }
+        }
+        return -1;
+    }
+};
