22cde24b09d84e1ea2492b60f60f3cdd450ae1a6
Harnoor Kaur Dhiman <161191385+harnoor8616@users.noreply.github.com>
2026-08-17 22:29:11 +0530

    Added solution - Time: O(n), Space: O(

---
+++ 703289-segregate-0s-and-1s-GFG/703289-segregate-0s-and-1s.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        // code here
+        int s=0,e=arr.size()-1;
+        while(s<e)
+        {
+            if(arr[s]==0)
+            {
+                s++;
+            }
+            else
+            {
+                if(arr[e]==0)
+                {
+                    swap(arr[s],arr[e]);
+                    s++;
+                    e--;
+                }
+                else 
+                {
+                    e--;
+                }
+            }
+        }
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
