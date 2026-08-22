5c47e14e4e89e60b65d3b94fb363a638b10392b4
Harnoor Kaur Dhiman <161191385+harnoor8616@users.noreply.github.com>
2026-08-22 21:29:17 +0530

    Time: 0 ms (100%), Space: 12.5 MB (81.73%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int>::iterator it =nums.begin();
-        vector<int>::iterator it1 =nums.begin();
+        vector<int> suffix(nums.size());
 
-        for (int i = 1; i < nums.size(); i++) {
-            nums[i] = nums[i]+nums[i - 1];
+        suffix[0] = nums[0];
+
+        for(int i = 1; i < nums.size(); i++) {
+            suffix[i] = suffix[i - 1] + nums[i];
         }
-        return nums;
+
+        return suffix;
     }
 };
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
